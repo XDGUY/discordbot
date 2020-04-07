261d7d39a661da278495f828854bee6962c37fc0
XDGUY <58673862+XDGUY@users.noreply.github.com>
2020-04-07 15:58:25 +0800

    message

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+const main = require("../index.js");
+
+module.exports = {
+    name:"pleh",
+    description:"Get help",
+    exec(m,args){
+    m.channel.send(main.embedFrom(main.name(), `help:information_source:`, 
+    `prefix is %(some commands start with %)
+    
+    throw: throw things
+    speak: I can speak to you`, `ffffff`));
+  }
+}
+
+//command not found type %pleh for more info:information_source:
